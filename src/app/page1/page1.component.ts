7404ab49b6d9e048f7798ec7ab4c613ef1aef464
ZConte <jesuzconte@gmail.com>
2023-05-09 12:38:38 -0400

    Adding Service

--- src/app/page1/page1.component.ts
+++ src/app/page1/page1.component.ts
@@ -1,21 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataService} from "../data.service";
+import {Book} from "../model/Book";
 
 @Component({
   selector: 'app-page1',
   templateUrl: './page1.component.html',
   styleUrls: ['./page1.component.css']
 })
-export class Page1Component {
+export class Page1Component implements OnInit {
   pageName = 'Page 1';
+  books?: Array<Book>;
 
-  onButtonClick() : void {
+  constructor(public dataService: DataService) {
+  }
+
+  ngOnInit() {
+    this.books = this.dataService.books;
+  }
+
+  onButtonClick(): void {
     alert('Hello');
   }
 
   // mouseOver() : void {
   //   alert('Your mouse is over this element!');
   // }
-
 
 
 }
